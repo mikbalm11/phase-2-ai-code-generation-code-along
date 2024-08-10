1fe4ed19a3f15241d49a843d44407c4bd3d620a2
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-08-09 22:11:45 -0400

    ai assist!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+
+/** A book object representation.
+ * @typedef {Object} Book
+ * @property {string} title - The title of the book.
+ * @property {string} author - The author of the book.
+ * @property {string} genre - The genre of the book.
+ */
+
+/** returns a list of books with the given genre.
+ * @param {string} genre - The genre of books required.
+ * @param {Book[]} books - An array of Book objects.
+ * @returns {Book[]} An array of Book objects matching the specified genre.
+ */
+
+function getBooksByGenre(genre, books) {
+    return books.filter((book) => book.genre === genre);
+}
